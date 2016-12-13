5438ee7417425d089a62190a7972ee58066e90f9
Bruno Furtado <brunofurtadoc@gmail.com>
2016-12-12 22:32:16 -0200

    Remove unused imports

--- app/src/main/java/io/github/bcfurtado/passpieforandroid/database/PasspieDatabase.java
+++ app/src/main/java/io/github/bcfurtado/passpieforandroid/database/PasspieDatabase.java
@@ -7,9 +7,6 @@ import android.util.Log;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
