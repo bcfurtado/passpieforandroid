99396728807d41081b289a0ad46c7bf83b66d6d5
Bruno Furtado <brunofurtadoc@gmail.com>
2017-06-08 16:51:05 -0300

    Add github log in

--- app/src/main/java/io/github/bcfurtado/passpieforandroid/intro/LoginActivity.java
+++ app/src/main/java/io/github/bcfurtado/passpieforandroid/intro/LoginActivity.java
@@ -1,15 +1,22 @@
 package io.github.bcfurtado.passpieforandroid.intro;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
 
 import io.github.bcfurtado.passpieforandroid.R;
 
@@ -43,11 +50,20 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         String password = passwordEditText.getText().toString();
 
         if (isValidLogin(login) && isValidPassword(password)) {
-            Intent it = new Intent(this, GenerateKeyActivity.class);
-            startActivity(it);
+            validateCredentials(login, password);
         } else {
             Toast.makeText(this, "Invalid login or password.", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void validateCredentials(String login, String password) {
+        ValidateCredentials va = new ValidateCredentials(this, login, password);
+        va.execute();
+    }
+
+    public void goToGenerateKeyActivity() {
+        Intent it = new Intent(this, GenerateKeyActivity.class);
+        startActivity(it);
     }
 
     private boolean isValidPassword(String password) {
@@ -56,6 +72,49 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private boolean isValidLogin(String login) {
         return login.length() >= 1;
+    }
+
+    public class ValidateCredentials extends AsyncTask {
+
+        private Context context;
+        private final String login;
+        private final String password;
+        private Boolean isCredentialValid;
+
+        private Exception exception;
+
+        public ValidateCredentials(Context context, String login, String password) {
+            this.context = context;
+            this.login = login;
+            this.password = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Object[] objects) {
+            try {
+                Log.d(ValidateCredentials.class.getSimpleName(), "Log in starting");
+                GitHub github = GitHub.connectUsingPassword(login, password);
+                isCredentialValid = github.isCredentialValid();
+                Log.d(ValidateCredentials.class.getSimpleName(), "Log in: isCredentialValid: " + isCredentialValid);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                this.exception = e;
+            }
+            Log.d(ValidateCredentials.class.getSimpleName(), "Log in finished.");
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object object) {
+            if (this.exception == null && isCredentialValid) {
+                Toast.makeText(context, "Valid credentials.", Toast.LENGTH_SHORT).show();
+                goToGenerateKeyActivity();
+            } else {
+                Toast.makeText(context, "Invalid credentials.", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 
 }
